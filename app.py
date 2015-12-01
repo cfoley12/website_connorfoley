6e2c32798a6a7df4e07992c88ff98d5b3d0bfc5d
Daniel Sack <sackd@0587361459.wireless.umich.net>
2015-11-30 19:38:21 -0500

    app 11/30 19:38

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ from flask import render_template
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+def get_resource_as_string(name, charset='utf-8'):
+    with app.open_resource(name) as f:
+        return f.read().decode(charset)
+
+app.jinja_env.globals['get_resource_as_string'] = get_resource_as_string
