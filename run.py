80885748c08e7f890f6de0da17db12b69baebbd8
Daniel Sack <sackd@0587361459.wireless.umich.net>
2015-11-30 19:39:59 -0500

    run 11/30 19:39

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from views import *
 if __name__ == '__main__':
     app.run(debug=True)
 
-<style type=text/css>{{ get_resource_as_string('static/stylesheet.css') }}</style>
+<style type=text/css>{{ get_resource_as_string('templates/stylesheetr.css') }}</style>
