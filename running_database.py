a31456c8efb741fe9cf6d230291b50fb0675a2b3
Daniel Sack <sackd@0587393241.wireless.umich.net>
2015-12-01 19:56:36 -0500

    all 12/01 19:56

--- running_database.py
+++ running_database.py
@@ -11,24 +11,24 @@ def get_courses():
     valid courses.
     '''
     #Define our connection string
-    conn_string = "host='' dbname='' user='' password=''"
+    conn_string = "host='pellefant-01.db.elephantsql.com' dbname='uncrgpvf' user='uncrgpvf' password='rkWkfGnFqOJsniQbLNAFk07p-XOEIz31'"
 
     # print the connection string we will use to connect
-    # print "Connecting to database\n ->%s" % (conn_string)
+    print "Connecting to database\n ->%s" % (conn_string)
 
     # get a connection, if a connect cannot be made an exception will be raised here
     conn = psycopg2.connect(conn_string)
 
     # conn.cursor will return a cursor object, you can use this cursor to perform queries
     cursor = conn.cursor()
-    # print "Connected!\n"
+    print "Connected!\n"
 
     # creating the query
-    # query =  "SELECT table_name  FROM information_schema.tables WHERE table_schema='public' AND column_name='race_name'   AND table_type='BASE TABLE';"
-    query =  "SELECT table_name FROM information_schema.columns WHERE column_name='race_name';"
+    # query =  "SELECT mis_states_2015  FROM information_schema.tables WHERE table_schema='public' AND column_name='identifier'   AND table_type='BASE TABLE';"
+    query =  "SELECT mis_states_2015 FROM information_schema.columns WHERE column_name='identifier';"
 
     cursor.execute(query,)
-    # conn.commit()
+    conn.commit()
 
     results = cursor.fetchall()
     return results
